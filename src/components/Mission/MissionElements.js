9e8867fafe5aa15f7414f8b9cbb1f862f9abbe5c
anwachuku15 <anwachuku15@gmail.com>
2021-10-11 15:38:13 -0400

    updated mission component

--- src/components/Mission/MissionElements.js
+++ src/components/Mission/MissionElements.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Doctor from "../../assets/images/doctor.jpeg";
+import Woman from "../../assets/images/womanyellow.jpeg";
 import Colors from "../../constants/Colors";
 
 export const MaskContainer = styled.div`
@@ -9,7 +9,7 @@ export const MaskContainer = styled.div`
   max-width: 1440px;
   height: 0px;
   padding-bottom: 50%;
-  z-index: 6;
+  z-index: -1;
 `;
 
 export const BackLayer = styled.div`
@@ -29,13 +29,16 @@ export const BackLayer = styled.div`
 export const BackLayerImage = styled.div`
   position: absolute;
   inset: 0px 0px 0px;
-  background-image: url(${Doctor});
-  background-position: center center;
+  mask-image: linear-gradient(to bottom, white, transparent);
+  background-image: url(${Woman});
+  background-position: 40% center;
   background-repeat: no-repeat;
   background-size: cover;
-  mask-image: linear-gradient(to bottom, white, transparent);
+  transform: ${({ offsetY }) => `translateY(${offsetY * -0.05}px)`};
+  /* transform: scaleX(-1); */
 `;
 export const MissionContainer = styled.section`
+  overflow-x: hidden;
   position: relative;
   width: 100%;
   max-width: 1440px;
@@ -45,6 +48,7 @@ export const MissionContainer = styled.section`
   transform-style: preserve-3d;
   background-color: ${Colors.primaryBlue};
   padding-bottom: 90px;
+  /* z-index: 10; */
 `;
 
 export const MissionWrapper = styled.div`
@@ -63,12 +67,19 @@ export const MissionWrapper = styled.div`
 export const MissionContentHeader = styled.div`
   position: relative;
   /* padding: 10rem 10px 0.5rem; */
-  padding: 25% 0 0;
-  margin-left: auto;
-  margin-right: auto;
+  padding: 320px 0 10% 10%;
+  /* margin-left: auto;
+  margin-right: auto; */
   text-align: center;
   color: white;
-  /* background: blue; */
-  width: 75%;
+  width: 100%;
+  /* background-color: blue; */
   /* margin-top: 40%; */
+`;
+
+export const MissionContentInfo = styled.div`
+  width: 100%;
+  z-index: 9;
+  background-image: linear-gradient(transparent);
+  /* background-color: ${Colors.primaryBlue}; */
 `;
--- src/components/Mission/index.js
+++ src/components/Mission/index.js
@@ -1,35 +1,37 @@
-import React from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+import React, { useState, useEffect } from "react";
+import { styled, createTheme } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
-import Colors from "../../constants/Colors";
 import {
   MissionContainer,
-  MaskContainer,
   MissionContentHeader,
   BackLayer,
   BackLayerImage,
+  MissionContentInfo,
 } from "./MissionElements";
 import SvgMask from "../MapView/SvgMask";
 import {
+  MaskContainer,
   MapViewContentContainer,
   MapViewContentWrapper,
   MapViewContentPopups,
-  MapViewContentInfo,
 } from "../MapView/MapViewElements";
+import AnimatedGrid from "./AnimatedGrid";
 
 const Mission = () => {
-  const Item = styled(Paper)(({ theme }) => ({
-    padding: 30,
-    textAlign: "center",
-    color: "white",
-    backgroundColor: "transparent",
-    boxShadow: "none",
-  }));
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.scrollY);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const HeaderText = styled(Paper)(({ theme }) => ({
-    padding: 30,
+    paddingTop: 30,
+    paddingLeft: 30,
+    // paddingRight: 30,
+    paddingBottom: 0,
     color: "white",
     backgroundColor: "transparent",
     boxShadow: "none",
@@ -38,132 +40,29 @@ const Mission = () => {
 
   return (
     <MissionContainer>
+      <BackLayer>
+        <BackLayerImage offsetY={offsetY} />
+      </BackLayer>
       <MaskContainer>
         <SvgMask backgroundColor={"white"} />
       </MaskContainer>
 
-      <BackLayer>
-        <BackLayerImage />
-      </BackLayer>
-
       <MapViewContentContainer>
         <MapViewContentWrapper>
-          <MissionContentHeader>
-            <Box sx={{ flexGrow: 1 }}>
-              <Grid item md={6} xs={12}>
-                <HeaderText style={{}}>
-                  <h2 style={{ fontSize: 48 }}>For the greater good.</h2>
-                </HeaderText>
-              </Grid>
-              <Grid item md={6} xs={12}>
-                <HeaderText>
-                  {/* <h2 style={{ fontSize: 48, color: "transparent" }}>
-                    For the greater good.
-                  </h2> */}
-                </HeaderText>
-              </Grid>
-            </Box>
-          </MissionContentHeader>
           <MapViewContentPopups>
             {/* <div style={{ height: 500 }}></div> */}
             <div style={{ height: 100 }}></div>
           </MapViewContentPopups>
 
-          <MapViewContentInfo>
-            <Box sx={{ flexGrow: 1 }}>
-              <Grid container spacing={2}>
-                <Grid item md={4} xs={12}>
-                  <Item
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "flex-start",
-                    }}
-                  >
-                    <div style={{ marginRight: 20 }}>
-                      <h2 style={{ fontSize: 20 }}>ICON</h2>
-                    </div>
-                    <div>
-                      <h2
-                        style={{
-                          fontSize: 20,
-                          textAlign: "left",
-                          paddingBottom: 16,
-                        }}
-                      >
-                        Get safety alerts when they matter to you - in real
-                        time.
-                      </h2>
-                      <p style={{ fontSize: 20, textAlign: "left" }}>
-                        Be situationally aware. Citizen alerts go out within
-                        seconds of a 911 or user report. If there's an incident,
-                        like a robery, nearby, you'll know to avoid that area.
-                      </p>
-                    </div>
-                  </Item>
-                </Grid>
-                <Grid item md={4} xs={12}>
-                  <Item
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "flex-start",
-                    }}
-                  >
-                    <div style={{ marginRight: 20 }}>
-                      <h2 style={{ fontSize: 20 }}>ICON</h2>
-                    </div>
-                    <div>
-                      <h2
-                        style={{
-                          fontSize: 20,
-                          textAlign: "left",
-                          paddingBottom: 16,
-                        }}
-                      >
-                        Know what’s happening so you and your loved ones can
-                        stay safe.
-                      </h2>
-                      <p style={{ fontSize: 20, textAlign: "left" }}>
-                        Connect with your family, friends, and neighbors on
-                        Citizen and find out when something important is
-                        happening around them.
-                      </p>
-                    </div>
-                  </Item>
-                </Grid>
-                <Grid item md={4} xs={12}>
-                  <Item
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "flex-start",
-                    }}
-                  >
-                    <div style={{ marginRight: 20 }}>
-                      <h2 style={{ fontSize: 20 }}>ICON</h2>
-                    </div>
-                    <div>
-                      <h2
-                        style={{
-                          fontSize: 20,
-                          textAlign: "left",
-                          paddingBottom: 16,
-                        }}
-                      >
-                        Always know why the helicopter is overhead.
-                      </h2>
-                      <p style={{ fontSize: 20, textAlign: "left" }}>
-                        If there’s commotion like police activity, helicopters
-                        overhead, or road closures, pull up the app and know why
-                        instantly.
-                      </p>
-                    </div>
-                  </Item>
-                </Grid>
-              </Grid>
-            </Box>
-          </MapViewContentInfo>
+          <MissionContentHeader>
+            <HeaderText style={{}}>
+              <h2 style={{ fontSize: 48 }}>Knowledge is power.</h2>
+            </HeaderText>
+          </MissionContentHeader>
+
+          <MissionContentInfo>
+            <AnimatedGrid />
+          </MissionContentInfo>
         </MapViewContentWrapper>
       </MapViewContentContainer>
     </MissionContainer>
